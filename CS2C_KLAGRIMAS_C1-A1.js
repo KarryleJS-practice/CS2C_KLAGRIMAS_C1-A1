a2bddd0be86530a681c95d93b48e1c9d195dd04c
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-09-23 21:00:50 +0800

    Create CS2C_KLAGRIMAS_C1-A1.js

---
+++ CS2C_KLAGRIMAS_C1-A1.js
@@ -0,0 +1,35 @@
+var name = "[Karryle Banasan Lagrimas]";
+var birthDate = "[July 20, 2005]";
+var birthPlace = "[San Fernando City La Union 2500]";
+var address = "[Bugnay, Candon City, Ilocos Sur, Philippines 2710]";
+var course = "[Bachelor Of Science in Computer Science]";
+var year = "2";
+var dreamJob = "[Data Analyst]";
+
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace .toUpperCase() + ", and currently living at " + address. toUpperCase()+ "."); 
+console.log(name. toLowerCase() + " is taking up " + course. toLowerCase() + " and dreams to be " + dreamJob + " after graduation.");
+
+var name = "[Jenny Saquiton Naungayan]";
+var birthDate = "[June 20, 2004]";
+var birthPlace = "[Oaig Daya Candon City Ilocos Sur 2710]";
+var address = "[Oaig Daya, Ilocos Sur, Philippines 2710]";
+var course = "[Bachelor Of Science in Computer Science]";
+var year = "2";
+var dreamJob = "[Software Engineer]";
+
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace .toUpperCase() + ", and currently living at " + address. toUpperCase()+ "."); 
+console.log(name. toLowerCase() + " is taking up " + course. toLowerCase() + " and dreams to be " + dreamJob + " after graduation.");
+
+var name = "[Renz Galcon Valdez]";
+var birthDate = "[April 13,2005]";
+var birthPlace = "[Resonable Hospital Candon Cuty Ilocos Sur Philippines 2710]";
+var address = "[Oaig Daya, Ilocos Sur, Philippines 2710]";
+var course = "[Bachelor Of Science in Computer Science]";
+var year = "2";
+var dreamJob = "[Game Developer]";
+
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace .toUpperCase() + ", and currently living at " + address. toUpperCase()+ "."); 
+console.log(name. toLowerCase() + " is taking up " + course. toLowerCase() + " and dreams to be " + dreamJob + " after graduation.");
